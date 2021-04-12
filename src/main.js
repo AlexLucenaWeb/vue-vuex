618af6e60e641963034ec01971cafa98fb084b98
janoweb <janoweb@users.noreply.github.com>
2021-04-12 11:15:41 +0100

    adding getters

--- src/main.js
+++ src/main.js
@@ -15,7 +15,22 @@ const store = createStore({
       state.counter = state.counter + 2;
     },
     increase(state, payload) {
-      state.counter = state.counter + payload.value ;
+      state.counter = state.counter + payload.value;
+    }
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+    normalizeCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
     }
   }
 });
