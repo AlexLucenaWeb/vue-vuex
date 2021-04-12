c5fb136f15bbc4fa37a36985ee70e63fc0640fb3
janoweb <janoweb@users.noreply.github.com>
2021-04-12 10:48:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="addTen">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter
+  },
+  methods: {
+    addTen() {
+      // this.$store.commit('increase', { value: 10 });
+      // New way to commit with and object:
+      this.$store.commit({
+        // the type is the name of the commit:
+        type: 'increase',
+        value: 10
+      })
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
